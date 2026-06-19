742600b332f071a311b18f8e721fdfb0003fb109
Thierbreezy <thiernosbdiallo@gmail.com>
2026-06-19 10:36:55 +0000

    MEAL-APP1

--- MEAL-APP1/App.js
+++ MEAL-APP1/App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import CategoriesScreen from './screens/CategogoriesScreen';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+      <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />      
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return (<>
+    <StatusBar style='dark'/>
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
